f2bcfa203bf74f436117f66f3e832936b0511c39
funnythingz <funnythingz69+github@gmail.com>
2016-01-18 13:22:41 +0900

    try goji

---
+++ app.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/zenazn/goji"
+	"github.com/zenazn/goji/web"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "index page")
+}
+
+func hello(c web.C, w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %s!", c.URLParams["name"])
+}
+
+func init() {
+	http.Handle("/", goji.DefaultMux)
+	goji.Get("/", index)
+	goji.Get("/hello/:name", hello)
+}
